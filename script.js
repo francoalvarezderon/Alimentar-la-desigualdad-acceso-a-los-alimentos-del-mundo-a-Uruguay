af2906b48a81f2d0bb2f8c363b0de0dbd6f4d16f
francoalvarezderon <francoalvarezderon53@gmail.com>
2026-07-17 16:07:31 -0300

    agrego lo nuevo

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const botonOrganismos = document.getElementById('btn-organismos');
+    const submenu = document.getElementById('submenu-organismos'); 
+
+    // Abre el submenu al hacer clic en el botón de Organismos
+    botonOrganismos.addEventListener('click', (evento) => { 
+        evento.preventDefault();
+        submenu.classList.toggle('mostrar');
+    }); 
+    
+
+    // Cierra el submenu al hacer clic en cualquier parte de la pantalla
+    document.addEventListener("click", (evento) => {
+        if (!botonOrganismos.contains(evento.target) && !submenu.contains(evento.target)) {
+            submenu.classList.remove("mostrar");
+        }
+    }); 
+
+});
+
