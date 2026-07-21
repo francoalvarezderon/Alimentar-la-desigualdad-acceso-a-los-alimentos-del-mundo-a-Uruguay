68a487fef9830157601b03bda81d13c28ecb1e05
francoalvarezderon <francoalvarezderon53@gmail.com>
2026-07-21 02:50:21 -0300

    Bastantes cambios nuevos, terminamos index.html

--- script.js
+++ script.js
@@ -6,14 +6,17 @@ document.addEventListener('DOMContentLoaded', () => {
     botonOrganismos.addEventListener('click', (evento) => { 
         evento.preventDefault();
         submenu.classList.toggle('mostrar');
-    }); 
     
-
+    
+        const estaAbierto = submenu.classList.contains('mostrar');
+        botonOrganismos.setAttribute('aria-expanded', estaAbierto);
+    }); 
     // Cierra el submenu al hacer clic en cualquier parte de la pantalla
     document.addEventListener("click", (evento) => {
         if (!botonOrganismos.contains(evento.target) && !submenu.contains(evento.target)) {
             submenu.classList.remove("mostrar");
-        }
+           botonOrganismos.setAttribute('aria-expanded', 'false');
+       }
     }); 
 
 });
